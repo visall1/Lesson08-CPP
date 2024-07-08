c5abc0194172685390a286309725054b5dfa298d
visall1 <khmercr12@gmail.com>
2024-07-08 20:05:21 +0700

    change code

--- OneWayList.cpp
+++ OneWayList.cpp
@@ -4,41 +4,84 @@ struct Node{
     string value;
     Node *next;
 };
+class OneWayList
+{
+private:
+    Node* pHead;
+    Node*pTail;
+    int Length;
+public:
+    OneWayList():pHead(nullptr),pTail(nullptr){}
+    void Add(string value){
+        Node* tmp = new Node{value,NULL};
+        if(pHead==NULL){
+            pTail = pHead = tmp;
+        }else{
+            pTail->next = tmp;
+            pTail = pTail->next;
+        }
+        Length++;
+    }
+    void Insert(int index,string value){
+        if(index == Length)return Add(value);
+        if(index<0)return;
+        
+        Node*tmp = new Node{value,NULL};
+        if(index == 0){
+            tmp->next = pHead;
+            pHead = tmp;
+        }else{
+            Node*p =pHead;
+            for(int i =1;i<index;i++,p=p->next){
+                tmp->next = p->next;
+                p->next = tmp;
+            }
+        }
+        Length++;
+    }
+    void Delete(int index){
+        if(Length==0)return;
+        if(index<0||index>=Length)return;
+
+        Node*tmp = pHead;
+        if(index==0){
+            pHead = tmp->next;
+            tmp->next = NULL;
+            delete tmp;
+
+        }else{
+            Node*p = pHead;
+            for(int i=1;i<Length;i++,p=p->next);
+                tmp = p->next;
+                p->next = tmp->next;
+                tmp->next = NULL;
+                if(pTail== tmp) pTail=p;
+                delete tmp;     
+        }
+        Length--;
+    }
+    void Print(){
+        for(Node*i=pHead;i!=NULL;i=i->next){
+            cout<<i->value<<" ";
+        }
+        cout<<endl;
+    }
+    int GetLength(){
+        return Length;
+    }
+};
+
 // This is the main function that demonstrates the creation and traversal of a one-way linked list.
 int main(){
-    // Create a new Node and assign it to the pointer pHead.
-    Node* pHead = new Node;
-
-    // Assign the value "Bill" to the value field of the Node pointed to by pHead.
-    pHead->value = "Bill";
-
-    // Set the next field of the Node pointed to by pHead to NULL, indicating the end of the list.
-    pHead->next = NULL;
-
-    // Create a new Node and assign it to the next field of the Node pointed to by pHead.
-    pHead->next = new Node;
-
-    // Assign the value "Mark" to the value field of the Node pointed to by pHead->next.
-    pHead->next->value = "Mark";
-
-    // Set the next field of the Node pointed to by pHead->next to NULL, indicating the end of the list.
-    pHead->next->next = NULL;
-
-    // Create a new Node and assign it to the next field of the Node pointed to by pHead->next.
-    pHead->next->next = new Node;
-
-    // Assign the value "John" to the value field of the Node pointed to by pHead->next->next.
-    pHead->next->next->value = "John";
-
-    // Set the next field of the Node pointed to by pHead->next->next to NULL, indicating the end of the list.
-    pHead->next->next->next = NULL;
-
-    // Create a Node pointer named i and assign it the value of pHead.
-    // This will be used to traverse the list.
-    for(Node* i=pHead; i!=NULL; i=i->next){
-        // Print the value of the Node pointed to by i.
-        cout<<i->value<<" ";
+    OneWayList list;
+    list.Add("Liza");
+    list.Add("Dima");
+    list.Add("Vova");
+    list.Insert(0,"Petya");
+    list.Print();
+    while(list.GetLength()>0){
+        list.Delete(0);
+        list.Print();
     }
-    // Return 0 to indicate successful execution.
     return 0;
 }
